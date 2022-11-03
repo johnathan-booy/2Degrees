0c16fc09f04325c9b58621b458c2a4472c286468
Johnathan Booy <johnny_music@icloud.com>
2022-11-03 10:14:41 -0700

    Connect database

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from models import db, connect_db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = "thisismysecret"
+
+connect_db(app)
+
+
+@app.route('/')
+def homepage():
+    """Show homepage"""
+    return render_template("base.html")
